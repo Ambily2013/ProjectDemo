be06b7e92a05ff5e38eed4607a1d4d684e527c88
Ambily2013 <ambilyap2013@gmail.com>
2023-02-17 15:49:15 +0530

    added

---
+++ ecommerceproject/shopapp/urls.py
@@ -0,0 +1,9 @@
+from .import views
+from django.urls import path
+app_name='shopapp'
+urlpatterns=[
+   # path('',views.index,name='index'),
+    path('',views.allProdCat,name='allProdCat'),
+    path('<slug:c_slug>/',views.allProdCat,name='products_by_category'),
+    path('<slug:c_slug>/<slug:product_slug>/',views.proDetail,name='proCatDetail'),
+]
